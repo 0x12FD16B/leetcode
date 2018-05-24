4534c577250ace4cf98c67dc9ba39822e09f1913
david-liu <recalleverything@163.com>
2018-05-24 17:11:31 +0800

    append problem #521 java solution

---
+++ algorithms/src/main/java/cn/antswl/leetcode/algorithms/longestUncommonSubsequenceI/Solution.java
@@ -0,0 +1,21 @@
+package cn.antswl.leetcode.algorithms.longestUncommonSubsequenceI;
+
+/**
+ * Longest Uncommon Subsequence I
+ *
+ * problem source: https://leetcode.com/problems/longest-uncommon-subsequence-i/description/
+ *
+ * @author david-liu
+ * @date May 24, 2018
+ */
+public class Solution {
+
+    // I think it's a full joke.
+    public int findLUSlength(String a, String b) {
+        if (!a.equals(b)) {
+            return Math.max(a.length(), b.length());
+        }
+        return -1;
+    }
+
+}

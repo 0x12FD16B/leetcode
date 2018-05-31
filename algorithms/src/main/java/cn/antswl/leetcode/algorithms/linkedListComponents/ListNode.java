3e8eeafd913aaac2e549b1a000a656d01a2368cc
david-liu <recalleverything@163.com>
2018-05-31 10:51:04 +0800

    append problem #817 java solution

---
+++ algorithms/src/main/java/cn/antswl/leetcode/algorithms/linkedListComponents/ListNode.java
@@ -0,0 +1,14 @@
+package cn.antswl.leetcode.algorithms.linkedListComponents;
+
+/**
+ * Definition for singly-linked list.
+ */
+class ListNode {
+    int val;
+
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
